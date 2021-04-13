93eb3568e2e781a2a9a600c6e34f78e96bb3e476
audreylauer <lauer.audrey@courrier.uqam.ca>
2021-04-13 10:50:34 -0400

    Test de pull

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# SCA7051 - Projet de modelisation d'un brouillard chaud
+# Auteurs: Audrey Lauer, Emile Cardinal
+# Date: 2021-04-13
+
+# Packages
 import numpy as np
+
+# Initialisation des variables
+
+# Constantes
+dt = 1 # sec
+pres = 1000e2 # Pa
+
+# Boucle temporelle
+
+
+
