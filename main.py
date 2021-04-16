b94f52836ce2d19d146c1e63e395119ea13a7139
audreylauer <lauer.audrey@courrier.uqam.ca>
2021-04-16 10:40:50 -0400

    OCD sur les commentaires

--- main.py
+++ main.py
@@ -5,9 +5,7 @@
 # Auteurs: Audrey Lauer, Émile Cardinal
 # Date de création : 2021-04-13
 # Date de remise : 2021-05-04
-#
-#########################
-#
+
 # Importation des packages
 import numpy as np
 import math
@@ -15,9 +13,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#
-########## Constantes d'entrées
-#
+# Constantes d'entrées
 dt = 1 # sec
 pres = 1000e2 # Pa
 taux_refroidissement = -2/(24*60*60) # K/s
@@ -27,19 +23,14 @@ Lv = 2.4656e6 # J/kg
 cp = 1005 # J/kg
 cv = cp - Rd
 
-#
-########## Variables initiales
-#
+# Variables initiales
 s = 1
 temperature = 15 + 273.15 # degC
 presvapsat = 1704.2 # Pa H2O, le esw
 presvap = 1704.2 # Pa H2O, le e
 qv = (Rd/Rv)*(presvap/pres) # kg H2O/kg air
 
-#
-########## Boucles temporelles
-#
-#
+# Boucles temporelles
 dt_list = []
 timerange = 28800*dt # 8 heures
 for t in range(timerange+1):
@@ -52,41 +43,32 @@ temperature_list.append(temperature) ## valeur initiale au pas de temps 0
 for i in range (timerange):
     temperature = temperature + taux_refroidissement
     temperature_list.append(temperature)  
-#
-#
+
 # Pression de vapeur saturante pour un pas de 8h (fonction de T)
 presvapsat_list = []
 presvapsat_list.append(presvapsat)
 for j in range (timerange):
     presvapsat = presvapsat + (Lv*presvapsat*taux_refroidissement/Rv/(temperature_list[j+1])**2)
     presvapsat_list.append(presvapsat)
-#
-#
-########### Tests
-#
-#
+
+# Tests
 print(temperature_list[0]) # Température initiale
 print(temperature_list[4320]) # T après 1.2 heures
 print(temperature_list[timerange]) # T après 8 heures
 print(presvapsat_list[0]) # esw initial
 print(presvapsat_list[4320]) # esw après 1.2 heures
 print(presvapsat_list[timerange]) # esw après 8 heures
-#
-#
-########### Graphiques
-#
-#
+
+# Graphiques
 # Pour temperature
 plot_temperature = plt.figure(1)
 plt.plot(dt_list,temperature_list)
-#
-#
+
 # Pour presvapsat
 plot_presvapsat = plt.figure(2)
 plt.plot(dt_list,presvapsat_list)
 
 plt.show()
-#
-#
+
 # Je me surprends moi même d'avoir réussi à faire ça. J'ai bel et bien checker pour esw et c'est pas une droite, mais une très légerte courbe.
 # J'ai fait le test en changeant dT/dt pour -20/86400, j'ai été récupéré la valeur de esw que j'obtenais pour T=14C, et je l'ai comparée avec la table des constantes, et c'est pareil (1597.6), donc ça marche!
