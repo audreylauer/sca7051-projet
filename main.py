f32b54b1526f21e16580a3d702722e0bcf6b0a2e
audreylauer <lauer.audrey@courrier.uqam.ca>
2021-05-04 17:19:19 -0400

    Travail ensemble
    - arrangement de la temperature quand on réchauffe
    - arrangement de masse_condensation
    - bug encore quand on réchauffe, masse_condensation augmente

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ version_prelim = False
 
 # Constantes d'entrées
 dt = 1 # sec
-timerange = (8*60*60)*dt # 8 heures
 pres = 1000e2 # Pa
 Rd = 287.04 # J/kgK
 Rv = 461.51 # J/kgK
@@ -50,6 +49,7 @@ else:
     masse_initial = 0
 
 # Boucle temporelle
+timerange = (12*60*60)*dt # 8 heures
 dt_list = np.arange(0, timerange+1, dt) # Pas de temps dans une liste
 taux_constant = False
 if taux_constant:
@@ -58,10 +58,11 @@ if taux_constant:
 elif not taux_constant:
     taux_refroidissement = np.zeros(timerange+1)
     temperature = np.zeros(timerange+1)
-    taux_1 = -10/(24*60*60)
-    temps_1 = 6*60*60
-    taux_2 = -10/(24*60*60)
-    temps_2 = 8*60*60
+    temperature[0] = temperature_initial
+    taux_1 = -5/(24*60*60)
+    temps_1 = 8*60*60
+    taux_2 = 5/(24*60*60)
+    temps_2 = 12*60*60
     for i in range(timerange+1):
         t = dt_list[i]
         if (t <= temps_1):
@@ -69,12 +70,16 @@ elif not taux_constant:
         elif (t > temps_1) and (i <= temps_2):
             taux_refroidissement[i] = taux_2
 
-        temperature[i] = temperature_initial + taux_refroidissement[i]
+        if i == 0:
+            temperature[i] = temperature_initial
+        else:
+            temperature[i] = temperature[i-1] + taux_refroidissement[i]*dt
 
 # Valeurs initiales
 N_CCN = [0]
 pres_vap = [pres_vap_initial]
 pres_vapsat = [pres_vapsat_initial]
+e_prime = [pres_vap_initial]
 C_prime = [0]
 C_double_prime = [0]
 P = [0]
@@ -82,6 +87,7 @@ S_double_prime = [1]
 rayon = [rayon_initial]
 masse_activation = [masse_initial]
 masse_condensation = [0]
+masse_precipitation = [0]
 
 # Début de la boucle temporelle
 for i in range(1,timerange+1): 
@@ -98,18 +104,21 @@ for i in range(1,timerange+1):
     rayon.append([])
     masse_activation.append([])
     masse_condensation.append([])
+    masse_precipitation.append([])
 
     # Variables pronostiques
     pres_vapsat[i] = pres_vapsat[i-1] + (Lv/Rv)*pres_vapsat[i-1]*taux_refroidissement[i]*dt/temperature[i]**2
 
-    # Calcul de S prelim
+    # Calcul de S prime
     P[i] = -S[i-1] * (Lv/Rv)/temperature[i-1]**2 * taux_refroidissement[i]
     S_prime = S[i-1] + P[i]*dt
     e_prime[i] = pres_vapsat[i] * S_prime
-    C_prime[i] = 1/pres_vapsat[i] * (e_prime[i] - e_prime[i-1])/dt
+    C_prime[i] = (1/pres_vapsat[i]) * (e_prime[i] - e_prime[i-1])/dt
     S_double_prime[i] = S[i-1] + (P[i] - C_prime[i])*dt
 
     # calculer la masse_condensation avec le C_prime
+    delta_masse_condensation = -1*(e_prime[i] - e_prime[i-1])/dt * Rd / (Rv*pres)
+    masse_condensation[i] = masse_condensation[i-1] + delta_masse_condensation*dt
 
     # Activation des aerosols (regarder ce qui reste qui n'a pas condensé)
     if version_prelim: # test sans jamais activer les aerosols
@@ -120,8 +129,10 @@ for i in range(1,timerange+1):
             C_ajuste = P[i] - (1 - S[i-1])/dt
             S[i] = S[i-1] + (P[i] - C_ajuste)*dt
             pres_vap[i] = pres_vapsat[i] * S[i]
-            delta_masse_condensation = (pres_vap[i] - pres_vap[i-1])/dt * Rd / (Rv*pres)
-            masse_condensation[i] = masse_condensation[i-1] + delta_masse_condensation*dt
+            #delta_masse_condensation = -1*(pres_vap[i] - pres_vap[i-1])/dt * Rd / (Rv*pres)
+            #masse_condensation[i] = masse_condensation[i-1] + delta_masse_condensation*dt
+
+            masse_activation[i] = 0.
 
         else: # activation (assez de vapeur)
             # Relation de Twomey
@@ -138,24 +149,28 @@ for i in range(1,timerange+1):
             if (S_double_prime[i] < 1):
                 C_ajuste = P[i] - (1 - S[i-1])/dt
                 pres_vap[i] = C_ajuste*pres_vapsat[i]*dt + pres_vap[i-1]
-                delta_masse_condensation = (pres_vap[i] - pres_vap[i-1])/dt * Rd / (Rv*pres)
-                masse_condensation[i] = masse_condensation[i-1] + delta_masse_condensation*dt
+           #     delta_masse_condensation = -1*(pres_vap[i] - pres_vap[i-1])/dt * Rd / (Rv*pres)
+           #     masse_condensation[i] = masse_condensation[i-1] + delta_masse_condensation*dt
                 S[i] = 1
 
             else:
                 S[i] = S_double_prime[i]
                 pres_vap[i] = pres_vapsat[i] * S[i]
-                delta_masse_condensation = (pres_vap[i] - pres_vap[i-1])/dt * Rd / (Rv*pres)
-                masse_condensation[i] = masse_condensation[i-1] + delta_masse_condensation*dt
+           #     delta_masse_condensation = -1*(pres_vap[i] - pres_vap[i-1])/dt * Rd / (Rv*pres)
+           #     masse_condensation[i] = masse_condensation[i-1] + delta_masse_condensation*dt
+  
+    # masse totale
+    masse_totale = masse_activation[i-1] + masse_condensation[i-1] + masse_precipitation[i-1]
 
     # Précipitation
-    vitesse = 1.19e6 * 100 * rayon[i-1]**2
-    masse_precipitation = vitesse * masse_activation[i] / H
-    N_precipitation = 3*masse_precipitation / ( rayon[i-1]**3 * 4*np.pi*rho_w )
+    vitesse = 1.19e8 * rayon[i-1]**2
+    delta_masse_precipitation = vitesse * masse_totale / H
+    masse_precipitation[i] = masse_precipitation[i-1] + delta_masse_precipitation*dt
+    N_precipitation = 3*masse_precipitation[i] / ( rayon[i-1]**3 * 4*np.pi*rho_w )
 
     # rayon final
-    #rayon[i] = ( (3 / (4*np.pi*rho_w)) * (masse_activation[i] + masse_condensation[i] + masse_precipitation)/(N_CCN[i] + N_precipitation) )**(1/3)
-    rayon[i] = ( (3 / (4*np.pi*rho_w)) * (masse_activation[i] + masse_precipitation)/(N_CCN[i] + N_precipitation) )**(1/3)
+    rayon[i] = ( (3 / (4*np.pi*rho_w)) * (masse_activation[i] + masse_condensation[i] + masse_precipitation[i])/(N_CCN[i] + N_precipitation) )**(1/3)
+#    rayon[i] = ( (3 / (4*np.pi*rho_w)) * (masse_activation[i] + masse_precipitation[i])/(N_CCN[i] + N_precipitation) )**(1/3)
 
 # Fin boucle temporelle
 
@@ -170,20 +185,20 @@ if test:
 
 # Graphiques
 # 
-plt.figure(1)
-plt.plot(dt_list[1:-1],S_double_prime[1:-1])
-plt.title('S_double_prime')
-
+#plt.figure(1)
+#plt.plot(dt_list[1:-1],S_double_prime[1:-1])
+#plt.title('S_double_prime')
 #
-plt.figure(2)
-plt.plot(dt_list,S)
-plt.title('S')
-
+##
+#plt.figure(2)
+#plt.plot(dt_list,S)
+#plt.title('S')
 #
-plt.figure(3)
-plt.plot(dt_list,pres_vapsat)
-plt.title('e_sw')
-
+##
+#plt.figure(3)
+#plt.plot(dt_list,pres_vapsat)
+#plt.title('e_sw')
+#
 ##
 #plt.figure(4)
 #plt.plot(dt_list,pres_vap)
